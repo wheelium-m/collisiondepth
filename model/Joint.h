5f60e6081163b17f651fe81748219a8dc7bb3009
Will <wmarshall484@gmail.com>
2012-06-26 15:46:24 -0400

    Added comments

--- model/Joint.h
+++ model/Joint.h
@@ -4,8 +4,10 @@
 class Joint{
  public:
   btTransform trans;
+  /*Points represent the centers of spheres*/
   btVector3 *points;
   int numPoints;
+  /*Radius of spheres*/
   float radius;
   Joint();
   Joint(btTransform, btVector3*,int, float);
