06c7dc9cebf44527915d3f82585bb0b303b75d93
Anthony Cowley <acowley@gmail.com>
2012-07-13 15:39:06 -0400

    Address warnings about initialization order.

--- include/Joint.h
+++ include/Joint.h
@@ -14,15 +14,15 @@ class Joint{
   /*Radius of spheres*/
   float radius;
   Joint() 
-    : name(""), trans(btTransform()), points(std::vector<btVector3>()), 
-      axis(btVector3()), radius(0.0f) {};
+    : name(""), trans(btTransform()), axis(btVector3()), 
+      points(std::vector<btVector3>()), radius(0.0f) {};
   Joint(const Joint& orig)
-    : name(orig.name), trans(orig.trans), points(orig.points), 
-      radius(orig.radius), axis(orig.axis) {}
+    : name(orig.name), trans(orig.trans), axis(orig.axis),
+      points(orig.points), radius(orig.radius) {}
   
   Joint(std::string n, btTransform t, std::vector<btVector3> pts, 
         btVector3 a, float r)
-    : name(n), trans(t), points(pts), axis(a), radius(r) {};
+    : name(n), trans(t), axis(a), points(pts), radius(r) {};
   //~Joint();
 };
 #endif
